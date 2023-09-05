57378d119b192a427748759a8652af9c37dbfb6d
Mohammad Tarek Hendi <t.hindi@outlook.com>
2023-09-05 13:24:47 +0800

    * Create Entity.

---
+++ app/src/main/java/com/example/offlinenoteapp/feature_note/domain/model/Note.kt
@@ -0,0 +1,29 @@
+package com.example.offlinenoteapp.feature_note.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.offlinenoteapp.core.ui.theme.BabyBlue
+import com.example.offlinenoteapp.core.ui.theme.LightGreen
+import com.example.offlinenoteapp.core.ui.theme.RedOrange
+import com.example.offlinenoteapp.core.ui.theme.RedPink
+import com.example.offlinenoteapp.core.ui.theme.Violet
+
+@Entity
+data class Note(
+    @PrimaryKey
+    val id: Int? = null,
+    val title: String,
+    val content: String,
+    val timestamp: Long,
+    val color: Int
+) {
+    companion object {
+        val noteColor = listOf(
+            RedOrange,
+            RedPink,
+            BabyBlue,
+            Violet,
+            LightGreen
+        )
+    }
+}
